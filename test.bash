2dafe6a0f0e37ec42f3df9a3f8b0637349e6fec9
Ryo Kozuka <ryokozu.fm710@sf7.so-net.ne.jp>
2023-11-29 12:08:10 +0900

    add even test

--- test.bash
+++ test.bash
@@ -11,7 +11,7 @@ res=0
 
 ###I/O TEST###
 out1=$(seq 5 | ./plus | sed -e 's/[^0-9]//g')
-out2=$(seq 5 | ./plus | sed -e 's/[0-9 :]//g
+out2=$(seq 5 | ./plus | sed 's/[^0-9]//g')
 [ "${out1}" = 15 ] || ng ${LINENO}
 [ "${out2}" = "奇数"] || ng ${LINENO}
 
