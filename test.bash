dfd6efbb19972f6966bef7ea481e6de95606f06d
Ryo Kozuka <ryokozu.fm710@sf7.so-net.ne.jp>
2023-11-29 11:51:53 +0900

    use sed

--- test.bash
+++ test.bash
@@ -10,8 +10,8 @@ ng () {
 res=0
 
 ###I/O TEST###
-out=$(seq 5 | ./plus)
-[ "${out} | sed -e 's/[^0-9]//g'" = 15 ] || ng ${LINENO}
+out=$(seq 5 | ./plus | sed -e 's/[^0-9]//g')
+[ "${out}" = 15 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
